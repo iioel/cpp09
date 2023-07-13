433eb3276abbd11883a146fddf136d10fb127e11
Yoel Cornamusaz <ycornamu@c1r10s4.42lausanne.ch>
2023-07-13 13:47:04 +0200

    Handle numbers more than INT_MAX

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ycornamu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 17:01:19 by ycornamu          #+#    #+#             */
-/*   Updated: 2023/07/11 17:01:26 by ycornamu         ###   ########.fr       */
+/*   Updated: 2023/07/13 13:41:59 by ycornamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(int ac, char **av)
 {
 	if (ac <= 2)
 	{
-		std::cerr << "Error: please give at least 2 postitive integers or more" << std::endl;
+		std::cerr << "Error: please give at least 2 postitive integers or more." << std::endl;
 		return (1);
 	}
 	PmergeMe pm;
@@ -28,9 +28,14 @@ int main(int ac, char **av)
 		{
 			if (!std::isdigit(av[i][j]))
 			{
-				std::cerr << "Error: please give only postitive integers" << std::endl;
+				std::cerr << "Error: please give only postitive integers." << std::endl;
 				return (1);
 			}
+		}
+		if (std::atof(av[i]) > INT_MAX)
+		{
+			std::cerr << "Error: number too high." << std::endl;
+			return (1);
 		}
 		pm.push_back(std::atoi(av[i]));
 	}
